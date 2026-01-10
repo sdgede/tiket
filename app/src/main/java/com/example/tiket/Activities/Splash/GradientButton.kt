0833a9be8c010d9457577b42a9cc87fbbf0cc3fc
sdgede <gede.indrawan49@smk.belajar.id>
2026-01-10 12:25:59 +0800

    finised splash screen

--- app/src/main/java/com/example/tiket/Activities/Splash/GradientButton.kt
+++ app/src/main/java/com/example/tiket/Activities/Splash/GradientButton.kt
@@ -1,2 +1,72 @@
 package com.example.tiket.Activities.Splash
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tiket.R
+
+
+@Composable
+@Preview
+fun GradientButton(
+    onClick: () -> Unit = {},
+    text: String = "Get Started",
+    padding: Int = 0
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(padding.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.Transparent
+        ),
+        elevation = ButtonDefaults.elevation(
+            defaultElevation = 0.dp,
+            pressedElevation = 0.dp,
+            disabledElevation = 0.dp,
+            hoveredElevation = 0.dp,
+            focusedElevation = 0.dp
+        )
+    ) {
+        Box(
+            modifier = Modifier
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            colorResource(id = R.color.purple),
+                            colorResource(id = R.color.pink)
+                        )
+                    ),
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .fillMaxWidth()
+                .padding(vertical = 12.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = text,
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/tiket/Activities/Splash/SplashActivity.kt
+++ app/src/main/java/com/example/tiket/Activities/Splash/SplashActivity.kt
@@ -1,17 +1,18 @@
 package com.example.tiket.Activities.Splash
 
-import android.R.attr.start
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -22,67 +23,108 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstrainedLayoutReference
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowCompat
+import com.example.tiket.MainActivity
 import com.example.tiket.R
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        enableEdgeToEdge()
+
+        setContent {
+            SplashScreen(onGetstartedClick = {
+                startActivity(Intent(this, MainActivity::class.java))
+            })
+        }
     }
 }
+
 @Composable
 @Preview
-fun SplashScreen() {
-    Column(modifier = Modifier.fillMaxSize ()) {
-        ConstraintLayout() {
-            val (backgroundImg, title, subtitle, startbtn) = createRefs()
-            Image(
-                painter = painterResource (R.drawable.splash_bg),
-                contentDescription = null,
-                modifier = Modifier
-                    .constrainAs(backgroundImg) {
-                        top.linkTo(parent.top)
-                        start.linkTo(parent.start)
-                    }
-                    .fillMaxSize()
-            )
-            val styledText = buildAnnotatedString {
-                append("Discover your\nDream")
-                withStyle(style = SpanStyle(color = colorResource(R.color.orange))){
-                    append(" Flight")
+fun SplashScreen(onGetstartedClick: () -> Unit = {}) {
+    StatusTopBarColor()
+
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val (backgroundImg, title, subtitle, startbtn) = createRefs()
+
+        // Background image full screen
+        Image(
+            painter = painterResource(R.drawable.splash_bg),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .constrainAs(backgroundImg) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom)
                 }
-                append("\nEasily")
+                .fillMaxSize()
+        )
+
+        val styledText = buildAnnotatedString {
+            append("Discover your\nDream")
+            withStyle(style = SpanStyle(color = colorResource(R.color.orange))) {
+                append(" Flight")
             }
-            Text(
-                text = styledText,
-                fontSize = 54.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.White,
-                modifier = Modifier
-                    .padding(top = 32.dp)
-                    .padding(horizontal = 16.dp)
-                    .constrainAs(title) {
-                        top.linkTo(backgroundImg.top)
-                        start.linkTo(backgroundImg.start)
-                    }
-            )
-            
-            Text(
-                text = stringResource(R.string.subtitle_splash),
-                fontSize = 16.sp,
-                color = colorResource(R.color.orange),
-                fontWeight = FontWeight.SemiBold,
-                modifier = Modifier
-                    .padding(top = 32.dp, start = 16.dp)
-                    .constrainAs(subtitle) {
-                        top.linkTo(title.bottom)
-                        start.linkTo(title.start)
-                    }
-            )
+            append("\nEasily")
         }
 
+        Text(
+            text = styledText,
+            fontSize = 54.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier
+                .padding(top = 60.dp)
+                .padding(horizontal = 16.dp)
+                .constrainAs(title) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                }
+        )
+
+        Text(
+            text = stringResource(R.string.subtitle_splash),
+            fontSize = 16.sp,
+            color = colorResource(R.color.orange),
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier
+                .padding(start = 16.dp, top = 16.dp)
+                .constrainAs(subtitle) {
+                    top.linkTo(title.bottom)
+                    start.linkTo(title.start)
+                }
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 40.dp)
+                .constrainAs(startbtn) {
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        ) {
+            GradientButton(onClick = onGetstartedClick, "Get Started", 32)
+        }
+    }
+}
+
+@Composable
+fun StatusTopBarColor() {
+    val systemUiController = rememberSystemUiController()
+
+    SideEffect {
+        systemUiController.setNavigationBarColor(
+            color = Color.Transparent,
+            darkIcons = false
+        )
     }
 }
